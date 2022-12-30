a04d5f2e9aa9695da267eb5f8ed1547baa4bb809
Castillo3004 <castillolj3004@gmail.com>
2022-12-30 14:29:40 -0500

    Se agrego componente dbz(interfaces, servicios, y mas)

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import { Personaje } from '../interfaces/dbz.interface';
+
+import { DbzService } from '../services/dbz.service';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+
+export class MainPageComponent {
+
+  // personajes: Personaje[]=[]
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: NaN
+  }
+
+  constructor(){
+    // this.personajes = this.dbzService.personajes;
+  }
+  // get personajes():Personaje[]{
+  //   return this.dbzService.personajes
+  // }
+
+  // agregarNuevoPersonaje(argumento: Personaje) {
+  //   this.personajes.push(argumento);
+  // }
+
+
+
+}
